b1f9f5712987c4639c4426e07a905e4b6a22056c
starterhub <starterhub@163.com>
2021-05-18 14:38:42 +0800

    xxx

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
+import CookieStore from './src/index'
+
 export interface ICookieStore {
   getRequestCookies: (domain:string, path: string) => string;
   setResponseCookies: (cookieStr: string, domain: string) => void;
 }
 
-export default class CookieStore {
-  getRequestCookies: (domain:string, path: string) => string;
-  setResponseCookies: (cookieStr: string, domain: string) => void;
-}
+export default CookieStore
