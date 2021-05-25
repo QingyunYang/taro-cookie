03dafa7e312c1941748e0d964fb452e4d3ee7b69
starterhub <starterhub@163.com>
2021-05-25 14:05:25 +0800

    新增拦截器

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import CookieStore from './CookieStore';
+import URLParse from 'url-parse'
+import { Chain, setStorageSync, getStorageSync, request } from '@tarojs/taro'
+import CookieStore from './CookieStore'
+
+export const cookieInterceptor = async(chain: Chain): Promise<any> => {
+  const cookieStore = new CookieStore({
+    storage: {
+      setItem: setStorageSync,
+      getItem: (key: string) => getStorageSync(key),
+    }
+  })
+
+  chain.requestParams.header = chain.requestParams.header || {}
+
+  const { hostname, pathname } = URLParse(chain.requestParams.url)
+  chain.requestParams.header['Cookie'] = cookieStore.getRequestCookies(hostname, pathname)
+
+  return chain.proceed(chain.requestParams).then((res: request.SuccessCallbackResult) => {
+    const cookieStr = res.header['Set-Cookie'] || res.header['set-cookie'] || ''
+    if (cookieStr) cookieStore.setResponseCookies(cookieStr, hostname)
+    return res
+  })
+}
 
 export default CookieStore;
